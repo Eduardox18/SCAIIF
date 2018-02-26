95c604125240b22bd4da80350f671a78274b76f5
Unknown <lalo_dd@hotmail.com>
2018-02-26 14:38:08 -0600

    Primer avance GUI CU01

---
+++ SCAIIF/src/controller/LoginController.java
@@ -0,0 +1,52 @@
+package controller;
+
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXPasswordField;
+import com.jfoenix.controls.JFXTextField;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+
+/**
+ *
+ * @author Ángel Eduardo Domínguez Delgado
+ */
+public class LoginController implements Initializable {
+    
+    @FXML
+    private JFXPasswordField campoContrasenia;
+
+    @FXML
+    private JFXTextField campoUsuario;
+
+    @FXML
+    private JFXButton botonSalir;
+
+    @FXML
+    private JFXButton botonIngresar;
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        
+    }    
+    
+    @FXML
+    void activarBotonIngresar() {
+        if (campoUsuario.getText().length() != 0 & campoContrasenia.getText().length() != 0) {
+            botonIngresar.setDisable(false);
+        } else {
+            botonIngresar.setDisable(true);
+        }
+    }
+
+    @FXML
+    void ingresarSistema() {
+        
+    }
+
+    @FXML
+    void salirSistema() {
+        System.exit(0);
+    }
+}
