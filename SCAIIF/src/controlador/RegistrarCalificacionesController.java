7c84fa811b60449bfe90bc781f619ccca1bb1263
Esmeralda Yamileth Hernández González <esmeyhg@gmail.com>
2018-03-07 12:33:20 -0600

    codificación CU11
    Registrar calificación

--- SCAIIF/src/controlador/RegistrarCalificacionesController.java
+++ SCAIIF/src/controlador/RegistrarCalificacionesController.java
@@ -1,8 +1,19 @@
 package controlador;
 
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXComboBox;
+import com.jfoenix.controls.JFXTextField;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
 
 /**
  * FXML Controller class
@@ -11,12 +22,56 @@ import javafx.fxml.Initializable;
  */
 public class RegistrarCalificacionesController implements Initializable {
 
+    @FXML
+    private JFXTextField TFMatricula;
+    @FXML
+    private JFXButton BTVolver;
+    @FXML
+    private JFXButton BTGuardar;
+    @FXML
+    private JFXButton BTCancelar;
+    @FXML
+    private JFXComboBox CBCurso;
+    @FXML
+    private JFXComboBox CBModulo;
+    @FXML
+    private JFXComboBox CBAutoevaluacion;
+    @FXML
+    private JFXTextField TFCalificacion;
+    
+    private SqlSessionFactory sqlSessionFactory = null;
+    
+    
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
+    } 
+    
+    @FXML
+    public void volverPrincipal () {
+        Stage stage = new Stage();
+        try {
+            Parent paginaPrincipal = FXMLLoader.load(getClass().getResource("/vista/Principal.fxml"));
+            Scene scene = new Scene(paginaPrincipal);
+            stage.setScene(scene);
+            stage.show();
+            Stage actualStage = (Stage) BTVolver.getScene().getWindow();
+            actualStage.close();
+        } catch (IOException ex) {
+            ex.printStackTrace();
+        }
+    }
+    
+    public void insertarCalificacion (double calificacion) {
+        
+        SqlSession session = sqlSessionFactory.openSession();
+        /*try {
+            int matricula = session.insert("CalificacionMapper.insertarCalificacion", calificacion);
+         
+        }*/
+    }
     
 }
