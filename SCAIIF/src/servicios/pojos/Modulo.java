677da49467257ddcca304a61d04a9194726c3d19
Esmeralda Yamileth Hernández González <esmeyhg@gmail.com>
2018-03-18 20:31:06 -0600

    Codificación CU11
    Registrar calificación

--- SCAIIF/src/servicios/pojos/Modulo.java
+++ SCAIIF/src/servicios/pojos/Modulo.java
@@ -7,8 +7,37 @@ package servicios.pojos;
 
 /**
  *
- * @author lalo
+ * @author esmeralda
  */
 public class Modulo {
+    private Integer idModulo;
+    private Integer noModulo;
+
+    public Modulo() {
+    }
+
+    public Modulo(Integer idModulo, Integer noModulo) {
+        this.idModulo = idModulo;
+        this.noModulo = noModulo;
+    }
+
+    public Integer getIdModulo() {
+        return idModulo;
+    }
+
+    public void setIdModulo(Integer idModulo) {
+        this.idModulo = idModulo;
+    }
+
+    public Integer getNoModulo() {
+        return noModulo;
+    }
+
+    public void setNoModulo(Integer noModulo) {
+        this.noModulo = noModulo;
+    }
+    
+    
+    
     
 }
