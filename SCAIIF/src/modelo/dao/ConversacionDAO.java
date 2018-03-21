088b2b39838ef29578d972b176fe036b9eabb368
Esmeralda Yamileth Hernández González <esmeyhg@gmail.com>
2018-03-20 21:56:49 -0600

    Finalización de separación en capas
    Casos de uso faltantes

--- SCAIIF/src/modelo/dao/ConversacionDAO.java
+++ SCAIIF/src/modelo/dao/ConversacionDAO.java
@@ -1,9 +1,35 @@
 package modelo.dao;
 
+import java.util.ArrayList;
+import java.util.List;
+import modelo.mybatis.MyBatisUtils;
+import org.apache.ibatis.session.SqlSession;
+import servicios.pojos.Conversacion;
+
 /**
  *
- * @author lalo
+ * @author esmeralda
  */
 public class ConversacionDAO {
-    
+
+    /**
+     * Recupera todas las conversaciones registradas en la base de datos.
+     *
+     * @return lista de conversaciones.
+     * @throws Exception
+     */
+    public static List<Conversacion> recuperarConversaciones() throws Exception {
+        List<Conversacion> conversaciones = new ArrayList<>();
+        SqlSession conn = null;
+        try {
+            conn = MyBatisUtils.getSession();
+            conversaciones = conn.selectList("Conversacion.getConversacion");
+
+        } finally {
+            if (conn != null) {
+                conn.close();
+            }
+        }
+        return conversaciones;
+    }
 }
