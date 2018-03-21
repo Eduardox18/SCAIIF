dca82225847c4bab9d58ec1f1539d7b5c87e53b6
Unknown <andresdglez@gmail.com>
2018-03-20 21:19:49 -0600

    Correción Observación pruebas Observación

--- SCAIIF/src/modelo/dao/ObservacionDAO.java
+++ SCAIIF/src/modelo/dao/ObservacionDAO.java
@@ -13,7 +13,16 @@ import servicios.pojos.Observacion;
  */
 public class ObservacionDAO {
 
+    /**
+     * Método para guardar una nueva observación en la base de datos
+     * @param observacion Objeto de tipo observación donde se encuentran todos los valores
+     * @return True en caso de que todo salga correcto, False en caso de que haya errores
+     * @throws Exception En caso de que exista una excepción el error se propaga a la capa superior
+     */
     public static boolean guardarObservacion(Observacion observacion) throws Exception {
+        if (observacion == null) {
+            return false;
+        }
         boolean resultado = false;
         SqlSession conn = null;
         try {
