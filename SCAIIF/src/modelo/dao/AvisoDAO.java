1083574a9164747ee6148ed6f2e4174d0ec5f582
Unknown <andresdglez@gmail.com>
2018-03-28 13:28:34 -0600

    Avance base datos

--- SCAIIF/src/modelo/dao/AvisoDAO.java
+++ SCAIIF/src/modelo/dao/AvisoDAO.java
@@ -1,9 +1,35 @@
 package modelo.dao;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.util.Date;
+import javafx.util.converter.LocalDateTimeStringConverter;
+import modelo.mybatis.MyBatisUtils;
+import modelo.pojos.Aviso;
+import org.apache.ibatis.session.SqlSession;
+
 /**
  *
  * @author lalo
  */
 public class AvisoDAO {
+    
+    public static boolean guardarAviso(Aviso aviso) throws Exception{
+        boolean resultado = false;
+        SqlSession conn = null;
+        
+        try{
+            conn = MyBatisUtils.getSession();
+            conn.insert("Aviso.guardarAviso", aviso);
+            conn.commit();
+            resultado = true;
+        } finally {
+            if(conn != null){
+                conn.close();
+            }
+        }
+        return resultado;
+    }
     
 }
