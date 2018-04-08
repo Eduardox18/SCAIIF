4546170ba9957ae3a5d72a00955ed19ed87f3a61
Esmeralda Yamileth Hernández González <esmeyhg@gmail.com>
2018-04-08 17:06:59 -0500

    Finalización Correcciones

--- SCAIIF/src/vista/controlador/RegistrodeAsistenciaController.java
+++ SCAIIF/src/vista/controlador/RegistrodeAsistenciaController.java
@@ -12,6 +12,8 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
@@ -55,7 +57,7 @@ public class RegistrodeAsistenciaController implements Initializable {
     @FXML
     private JFXComboBox<String> comboFecha;
     @FXML
-    private JFXComboBox<Integer> comboActividad;
+    private JFXComboBox<String> comboActividad;
     @FXML
     JFXHamburger menuIcon = new JFXHamburger();
     @FXML
@@ -72,7 +74,7 @@ public class RegistrodeAsistenciaController implements Initializable {
             menuDrawer.setDisable(true);
         } catch (IOException ex) {
             Dialogo dialogo = new Dialogo(Alert.AlertType.ERROR,
-                    "Servidor no disponible, intente más tarde", "Error", ButtonType.OK);
+                "Servidor no disponible, intente más tarde", "Error", ButtonType.OK);
             dialogo.show();
         }
         menuIcon.addEventHandler(MouseEvent.MOUSE_CLICKED, (e) -> {
@@ -102,31 +104,45 @@ public class RegistrodeAsistenciaController implements Initializable {
      */
     private void llenarComboBox() {
         List<Reservacion> fechas = null;
-        List<Actividad> noActividad = null;
         Dialogo dialogo = null;
         try {
             fechas = ReservacionDAO.recuperarFechas();
             List<String> fechaReservacion = new ArrayList<>();
             for (Reservacion reservacion : fechas) {
-                SimpleDateFormat formatter = new SimpleDateFormat("dd/MM/yyyy");
+                SimpleDateFormat formatter = new SimpleDateFormat("yyyy/MM/dd");
                 String fechaTexto = formatter.format(reservacion.getFecha());
                 fechaReservacion.add(fechaTexto);
             }
             ObservableList<String> fechasObservable = FXCollections.observableArrayList(fechaReservacion);
             comboFecha.setItems(fechasObservable);
+        } catch (IOException ex) {
+            dialogo = new Dialogo(Alert.AlertType.ERROR,
+                "Error al recuperar fechas.", "Error", ButtonType.OK);
+            dialogo.show();
+        }
+    }
 
-            noActividad = ActividadDAO.recuperarNoActividad();
-            List<Integer> noActividades = new ArrayList<>();
-            for (Actividad actividad : noActividad) {
-                noActividades.add(actividad.getNoActividad());
+    /**
+     * Recupera el nombre de las actividades de acuerdo a la fecha seleccionada.
+     */
+    @FXML
+    public void recuperarActividades() {
+        List<Actividad> nombreActividad = null;
+        Dialogo dialogo = null;
+        try {
+            nombreActividad = ActividadDAO.recuperarNombreActividad(comboFecha.getSelectionModel().getSelectedItem());
+            List<String> nombreActividades = new ArrayList<>();
+            for (Actividad actividad : nombreActividad) {
+                nombreActividades.add(actividad.getNombre());
             }
-            ObservableList<Integer> noActividadObservable = FXCollections.observableArrayList(noActividades);
+            ObservableList<String> noActividadObservable = FXCollections.observableArrayList(nombreActividades);
             comboActividad.setItems(noActividadObservable);
         } catch (IOException ex) {
             dialogo = new Dialogo(Alert.AlertType.ERROR,
-                "Error al recuperar información.", "Error", ButtonType.OK);
+                "Error al recuperar nombre de actividades.", "Error", ButtonType.OK);
             dialogo.show();
         }
+
     }
 
     /**
@@ -135,22 +151,34 @@ public class RegistrodeAsistenciaController implements Initializable {
     @FXML
     public void recuperarLista() {
         Dialogo dialogo = null;
-        int noActividad = comboActividad.getValue();
         Date fechaDate = null;
+        List<Integer> noActividades = null;
         String fecha = comboFecha.getValue();
-        SimpleDateFormat fechaFormat = new SimpleDateFormat("dd/MM/yyyy");
+        SimpleDateFormat fechaFormat = new SimpleDateFormat("yyyy/MM/dd");
+        List<Actividad> noActividad = null;
+        int numActividad = 0;
+
         try {
             fechaDate = fechaFormat.parse(fecha);
-        } catch (ParseException ex) {
-            dialogo = new Dialogo(Alert.AlertType.ERROR,
-                "Error al recuperar actividades", "Error", ButtonType.OK);
-            dialogo.show();
+        } catch (ParseException ex) { 
+        }
+
+        try {
+            noActividad = ActividadDAO.recuperarNombreActividad(comboFecha.getSelectionModel().getSelectedItem());
+            noActividades = new ArrayList<>();
+            for (Actividad actividad : noActividad) {
+                noActividades.add(actividad.getNoActividad());
+            }
+            numActividad = noActividades.get(comboActividad.getSelectionModel().getSelectedIndex());
+            
+            System.out.println("noActv: " + noActividades.get(comboActividad.getSelectionModel().getSelectedIndex()));
+        } catch (IOException | ArrayIndexOutOfBoundsException ex) {
         }
 
         List<Alumno> reservaciones = new ArrayList<>();
         Reservacion reservacion = new Reservacion();
         reservacion.setFecha(fechaDate);
-        reservacion.setNoActividad(noActividad);
+        reservacion.setNoActividad(numActividad);
         reservacion.setAsistencia(false);
 
         try {
@@ -189,21 +217,33 @@ public class RegistrodeAsistenciaController implements Initializable {
     private void comprobarRegistroAsistencia() {
         Dialogo dialogo = null;
         String matricula = tablaAlumnos.getSelectionModel().getSelectedItem().toString();
-        int noActividad = comboActividad.getValue();
         Date fechaDate = null;
+        List<Actividad> noActividad = null;
+        List<Integer> noActividades = null;
+        int numActividad = 0;
+
         String fecha = comboFecha.getValue();
-        SimpleDateFormat fechaFormat = new SimpleDateFormat("dd/MM/yyyy");
+        SimpleDateFormat fechaFormat = new SimpleDateFormat("yyyy/MM/dd");
+
+        try {
+            noActividad = ActividadDAO.recuperarNombreActividad(comboFecha.getSelectionModel().getSelectedItem());
+            noActividades = new ArrayList<>();
+            for (Actividad actividad : noActividad) {
+                noActividades.add(actividad.getNoActividad());
+            }
+            numActividad = noActividades.get(comboActividad.getSelectionModel().getSelectedIndex());
+        } catch (IOException | ArrayIndexOutOfBoundsException ex) {
+        }
+
         try {
             fechaDate = fechaFormat.parse(fecha);
         } catch (ParseException ex) {
-            dialogo = new Dialogo(Alert.AlertType.ERROR,
-                "Error al recuperar actividades", "Error", ButtonType.OK);
-            dialogo.show();
         }
+        
         Reservacion reservacion = new Reservacion();
         reservacion.setMatricula(matricula);
-        reservacion.setNoActividad(noActividad);
         reservacion.setFecha(fechaDate);
+        reservacion.setNoActividad(numActividad);
         reservacion.setAsistencia(true);
         try {
             ReservacionDAO.registrarAsistencia(reservacion);
@@ -227,7 +267,7 @@ public class RegistrodeAsistenciaController implements Initializable {
      */
     public void limpiarCampos() {
         comboFecha.setValue("");
-        comboActividad.setValue(0);
+        comboActividad.setValue("");
     }
 
     /**
