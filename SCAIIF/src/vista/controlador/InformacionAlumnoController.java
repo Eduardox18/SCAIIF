7793081594e9c20fc327f4a8d8289ed62e2c244a
Esmeralda Yamileth Hernández González <esmeyhg@gmail.com>
2018-04-22 00:41:36 -0500

    InterfazCU13

---
+++ SCAIIF/src/vista/controlador/InformacionAlumnoController.java
@@ -0,0 +1,61 @@
+package vista.controlador;
+
+import com.jfoenix.controls.JFXDrawer;
+import com.jfoenix.controls.JFXHamburger;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.VBox;
+import vista.Dialogo;
+
+/**
+ * FXML Controller class
+ *
+ * @author esmeralda yamileth Hernández González
+ */
+public class InformacionAlumnoController implements Initializable {
+
+    @FXML
+    JFXHamburger menuIcon = new JFXHamburger();
+    @FXML
+    JFXDrawer menuDrawer = new JFXDrawer();
+
+    /**
+     * Initializes the controller class.
+     */
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        try {
+            VBox box = FXMLLoader.load(getClass().getResource("/vista/DrawerPrincipal.fxml"));
+            menuDrawer.setSidePane(box);
+            menuDrawer.setDisable(true);
+        } catch (IOException ex) {
+            Dialogo dialogo = new Dialogo(Alert.AlertType.ERROR,
+                "Servidor no disponible, intente más tarde", "Error", ButtonType.OK);
+            dialogo.show();
+        }
+        menuIcon.addEventHandler(MouseEvent.MOUSE_CLICKED, (e) -> {
+            menuDrawer.open();
+            menuDrawer.setDisable(false);
+            menuIcon.setVisible(false);
+        });
+    }
+
+    /**
+     * Método que hace visible e invisible el menú drawer.
+     */
+    @FXML
+    public void mostrarIcono() {
+        if (!menuDrawer.isShown()) {
+            menuIcon.setVisible(true);
+            menuDrawer.setDisable(true);
+        }
+    }
+
+}
