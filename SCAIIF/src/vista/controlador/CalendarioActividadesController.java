2491f8c3f26771ea7554d362aba2a93fdec38a96
Unknown <andresdglez@gmail.com>
2018-05-05 19:22:34 -0500

    Correcciones CU

--- SCAIIF/src/vista/controlador/CalendarioActividadesController.java
+++ SCAIIF/src/vista/controlador/CalendarioActividadesController.java
@@ -1,27 +1,80 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package vista.controlador;
 
+import com.jfoenix.controls.JFXComboBox;
+import com.jfoenix.controls.JFXDrawer;
+import com.jfoenix.controls.JFXHamburger;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.Label;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.VBox;
+import modelo.pojos.ActividadAsesor;
+import modelo.pojos.Curso;
+import vista.Dialogo;
 
-/**
- * FXML Controller class
- *
- * @author José Andrés Domínguez González
- */
 public class CalendarioActividadesController implements Initializable {
+    
+     @FXML
+    private TableView<ActividadAsesor> tablaActividades;
+
+    @FXML
+    private TableColumn columnaFecha;
+
+    @FXML
+    private TableColumn columnaNombre;
+
+    @FXML
+    private JFXComboBox<String> comboMes;
+
+    @FXML
+    private JFXComboBox<Curso> comboCurso;
+
+    @FXML
+    private JFXDrawer menuDrawer;
+
+    @FXML
+    private JFXHamburger menuIcon;
 
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        // TODO
+        try {
+            VBox box = FXMLLoader.load(getClass().getResource("/vista/DrawerPrincipal.fxml"));
+            menuDrawer.setSidePane(box);
+            menuDrawer.setDisable(true);
+        } catch (IOException ex) {
+            Dialogo dialogo = new Dialogo(Alert.AlertType.ERROR,
+                    "Servidor no disponible, intente más tarde", "Error", ButtonType.OK);
+            dialogo.show();
+        }
+        menuIcon.addEventHandler(MouseEvent.MOUSE_CLICKED, (e) -> {
+            menuDrawer.open();
+            menuDrawer.setDisable(false);
+            menuIcon.setVisible(false);
+        });
+        
+        tablaActividades.setPlaceholder(new Label("No hay actividades disponibles"));
     }    
+    
+    /**
+     * Muestra el ícono del menú en la ventana
+     */
+    @FXML
+    public void mostrarIcono() {
+        if (!menuDrawer.isShown()) {
+            menuIcon.setVisible(true);
+            menuDrawer.setDisable(true);
+        }
+    }
     
 }
