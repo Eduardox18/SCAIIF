f71ca3d9b5aee48d8d1643e84c782c99aaebd12d
Esmeralda Yamileth Hernández González <esmeyhg@gmail.com>
2018-05-07 00:50:15 -0500

    Avances CU12
    Interfaces y consultas

--- SCAIIF/src/modelo/dao/ActividadDAO.java
+++ SCAIIF/src/modelo/dao/ActividadDAO.java
@@ -13,11 +13,12 @@ import modelo.pojos.ActividadAsesor;
 public class ActividadDAO {
 
     /**
-     * Recupera el historial de actividades que realizará o realizó el Asesor
-     * con el identificador de noPersonal determinado. 
+     * Recupera el historial de actividades que realizará o realizó el Asesor con el identificador
+     * de noPersonal determinado.
+     *
      * @param noPersonal identificador del Asesor.
      * @return lista de las actividades realizadas y por realizar del Asesor.
-     * @throws Exception 
+     * @throws Exception
      */
     public static List<Actividad> recuperarHistorial(int noPersonal) throws Exception {
         SqlSession conn = null;
@@ -35,10 +36,11 @@ public class ActividadDAO {
 
     /**
      * Método que permite recuperar las actividades asociadas a un asesor
+     *
      * @param noPersonal Número de personal del asesor
      * @param fecha Fecha actual
      * @return Regresa una lista de actividades
-     * @throws Exception 
+     * @throws Exception
      */
     public static List<Actividad> recuperarActividadesAsesor(int noPersonal, Date fecha)
         throws Exception {
@@ -61,8 +63,9 @@ public class ActividadDAO {
 
     /**
      * Recupera el nombre de cada noActividad registrado en la base de datos.
-     * @return 
-     * @throws java.io.IOException 
+     *
+     * @return
+     * @throws java.io.IOException
      */
     public static List<Actividad> recuperarNombreActividad(String fecha) throws IOException {
         List<Actividad> nombreActividad = new ArrayList();
@@ -77,82 +80,105 @@ public class ActividadDAO {
         }
         return nombreActividad;
     }
-    
+
     /**
      * Recupera una lista de las actividades pendientes
+     *
      * @return List que contiene las actividades pendientes que no se encuentren canceladas
-     * @throws IOException 
+     * @throws IOException
      */
     public static List<ActividadAsesor> recuperarActividadesPendientes() throws IOException {
         List<ActividadAsesor> actividadesPendientes;
-        try(SqlSession conn = MyBatisUtils.getSession()){
+        try (SqlSession conn = MyBatisUtils.getSession()) {
             actividadesPendientes = conn.selectList("Actividad.getActividadesPendientes");
-        } 
-        
+        }
+
         return actividadesPendientes;
     }
-    
+
+    /**
+     * Recupera una lista de las actividades por impartir
+     *
+     * @return List que contiene las actividades pendientes que no se encuentren canceladas
+     * @throws IOException
+     */
+    public static List<ActividadAsesor> recuperarActividadesPorImpartir(Integer noPersonal,
+        Date fecha) throws IOException {
+        List<ActividadAsesor> actividadesPorImpartir;
+        HashMap<String, Object> params = new HashMap<>();
+        params.put("noPersonal", noPersonal);
+        params.put("fecha", fecha);
+        try (SqlSession conn = MyBatisUtils.getSession()) {
+            actividadesPorImpartir = conn.selectList("Actividad.getActividadesPorImpartir", params);
+        }
+
+        return actividadesPorImpartir;
+    }
+
     /**
      * Actualiza el estado de una actividad
+     *
      * @param noActividad La actividad a la que se desea actualizar el estado
      * @param estado Número que representa el estado que tendrá la actividad
-     * @return 
-     * @throws IOException 
+     * @return
+     * @throws IOException
      */
-    public static boolean actualizarEstado (Integer noActividad, Integer estado) throws IOException{
+    public static boolean actualizarEstado(Integer noActividad, Integer estado) throws IOException {
         boolean resultado = false;
         HashMap<String, Integer> params = new HashMap<>();
         params.put("noActividad", noActividad);
         params.put("idEstado", estado);
-        
-        try(SqlSession conn = MyBatisUtils.getSession()){
+
+        try (SqlSession conn = MyBatisUtils.getSession()) {
             conn.update("Actividad.cambiarEstado", params);
             conn.commit();
             resultado = true;
-        } 
+        }
         return resultado;
     }
-    
+
     /**
      * Devuelve las actividades en las que no se haya inscrito un alumno determinado
+     *
      * @param matricula La matrícula del alumno
      * @param nrc
      * @return Lista con las actividades disponibles
-     * @throws Exception 
+     * @throws Exception
      */
     public static List<ActividadAsesor> recuperarActividadesDisponibles(String matricula, Integer nrc) throws Exception {
-        List <ActividadAsesor> listaActividades;
+        List<ActividadAsesor> listaActividades;
         HashMap<String, Object> params = new HashMap<>();
         params.put("matricula", matricula);
         params.put("nrc", nrc);
-        
-        try(SqlSession conn = MyBatisUtils.getSession()){
+
+        try (SqlSession conn = MyBatisUtils.getSession()) {
             listaActividades = conn.selectList("Actividad.actividadesDisponibles", params);
         }
-        
-        return listaActividades;
-    } 
-    
-    /**
-     * Recupera las actividades de un curso
-     * @param nrc El nrc del curso del que se desean recuperar las actividades
-     * @return Lista con las actividades del curso
-     * @throws Exception 
-     */
-    public static List<ActividadAsesor> recuperarActividadesCalendario(Integer nrc) 
-            throws Exception {
-        List <ActividadAsesor> listaActividades;
-        
-        try(SqlSession conn = MyBatisUtils.getSession()){
-            listaActividades = conn.selectList("Actividad.actividadesCalendario", nrc);
-        }
-        
+
         return listaActividades;
     }
-    
+
+    /**
+     * Recupera las actividades de un curso
+     *
+     * @param nrc El nrc del curso del que se desean recuperar las actividades
+     * @return Lista con las actividades del curso
+     * @throws Exception
+     */
+    public static List<ActividadAsesor> recuperarActividadesCalendario(Integer nrc)
+        throws Exception {
+        List<ActividadAsesor> listaActividades;
+
+        try (SqlSession conn = MyBatisUtils.getSession()) {
+            listaActividades = conn.selectList("Actividad.actividadesCalendario", nrc);
+        }
+
+        return listaActividades;
+    }
+
     public static ActividadAsesor detalleActividad(Integer noActividad) throws Exception {
         ActividadAsesor detalleActividad;
-        try(SqlSession conn = MyBatisUtils.getSession()){
+        try (SqlSession conn = MyBatisUtils.getSession()) {
             detalleActividad = conn.selectOne("Actividad.detalleActividad", noActividad);
         }
         return detalleActividad;
